8c07ea17a9c091b8bf23a6c0597fa12262a90d44
Viliam Kubis <viliam.kubis@gmail.com>
2018-02-15 19:52:11 +0100

    updates

--- generate_last_days_regex.php
+++ generate_last_days_regex.php
@@ -1,5 +1,8 @@
 <?php
 
+// uncomment to import all time payouts history for the first time
+// die('([0-9]{4}-[0-9]{2}[0-9]{2})');
+
 $date = new DateTime('tomorrow');
 
 echo '(' . $date->format('Y-m-d') . '|';
