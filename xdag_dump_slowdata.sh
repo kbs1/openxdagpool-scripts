bb48bf6cf36385b117a4d1616f8519eddc8858eb
Viliam Kubis <viliam.kubis@gmail.com>
2018-04-19 22:49:09 +0200

    payouts grep: process binary files as text

--- xdag_dump_slowdata.sh
+++ xdag_dump_slowdata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-grep -Ph "`/usr/bin/php /home/pool/scripts/generate_last_days_regex.php`"'.+:MESS]  Xfer  : from [0-9a-zA-Z+/]{32}' /home/pool/xdag1/client/xdag.log /home/pool/xdag2/client/xdag.log | sort > /var/www/pool/payouts_tmp.txt
+grep -Pha "`/usr/bin/php /home/pool/scripts/generate_last_days_regex.php`"'.+:MESS]  Xfer  : from [0-9a-zA-Z+/]{32}' /home/pool/xdag1/client/xdag.log /home/pool/xdag2/client/xdag.log | sort > /var/www/pool/payouts_tmp.txt
 
 sleep 1
 
